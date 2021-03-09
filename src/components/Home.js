2d41e21abfd1d02d4e7c06d67a705388541f0b01
Kumar Deepanshu <kumardeepanshu157@gmail.com>
2021-03-09 22:41:59 +0530

    sidebar done

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+
+import './Home.css'
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__main">
+
+
+            </div>       
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/SideBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Sidebar.css'
+
+function SideBar() {
+    return (
+        <div className="sidebar">
+        <div className="sidebar__main">
+            
+        </div>
+            
+        </div>
+    )
+}
+
+export default SideBar
